29a2349b2f0e976db407564b33659d059e9de2e7
SHRIMPTAILS_\shrim <shrimptail.f3@gmail.com>
2026-09-05 02:03:32 +0900

    first commit

--- infra/stacks/application.go
+++ infra/stacks/application.go
@@ -196,7 +196,7 @@ func grantDeploymentPermissions(project awscodebuild.PipelineProject, cfg config
 		Resources: &functionArns,
 	}))
 	project.AddToRolePolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
-		Actions: _jsii_.Strings("codedeploy:CreateDeployment", "codedeploy:GetDeployment"),
+		Actions: _jsii_.Strings("codedeploy:CreateDeployment", "codedeploy:GetDeployment", "codedeploy:GetDeploymentConfig"),
 		Resources: func() *[]*string {
 			resources := append(deploymentGroupArns, _jsii_.String("*"))
 			return &resources
